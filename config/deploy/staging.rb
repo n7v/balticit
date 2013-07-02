77cde50f4c8b3ccd96a451b6c2df5149666da307
novoselov <novsem@gmail.com>
2013-07-02 14:21:18 +0300

    deploy: fix staging - add cloning of production db and images

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,12 @@ role :db,  "stage.balticit.ru", :primary => true
 set :rails_env, "staging"
 set :branch, "stage"
 
-set :deploy_to, "/var/www/apps/#{application}_#{stage}"
+set :deploy_to, "#{base_directory}/#{application}_#{stage}"
 
 set :keep_releases, 5
+
+set :current_path, File.join(deploy_to, current_dir) #fix for capistrano-unicorn
+
+after 'deploy:update_code', 'db:clone_production'
+after 'db:clone_production', 'deploy:migrate'
+after 'deploy:update_code', 'db:rsync_system'
