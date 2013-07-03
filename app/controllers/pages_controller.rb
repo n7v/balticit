a472ca884b41cdf031bf8c41084c93e5ccdd655a
Nastya <nastya@Lemon.(none)>
2013-07-03 14:32:23 +0300

    pravka

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
 layout :layout
+before_filter :menu_main 
 
   def show
     @projects = Project.all
@@ -10,6 +11,14 @@ layout :layout
       puts 'no'
     end
   end
+
+  def menu_main 
+    @page = Page.find_by_slug(params['slug'])
+    @top_menu = @page.root.siblings.where(hidden: false) 
+    @middle_menu = @page.ancestors.where(hidden: false)
+    @bottom_menu = @page.children.where(hidden: false)
+    @main_menu = @page.siblings.where(hidden: false)  
+  end 
 
   private
     def layout
